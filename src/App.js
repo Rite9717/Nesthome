5d9c162f2bef1b4d9ad82843855eeb3931c57ca2
Rite9717 <riteshnkumar261@gmail.com>
2025-02-03 01:14:27 +0530

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Footer } from "./Pages/Footer";
+import { NavBar } from "./Pages/NavBar";
+
+function App(){
+  return (
+    <div className="App">
+      <NavBar/>
+      <div className="content">
+        <h1>Nigga</h1>
+      </div>
+      <Footer/>
+    </div>
+    
+  )
+}
+
+export default App;
